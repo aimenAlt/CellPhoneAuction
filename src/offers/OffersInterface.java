a2878fe82704ccb938e276cedfba9440481488b9
aimenAlt <work@Aimens-MacBook-Pro-2.local>
2021-10-27 12:55:13 -0700

    Create Offer List Interface

---
+++ src/offers/OffersInterface.java
@@ -0,0 +1,19 @@
+package offers;
+
+import java.util.HashMap;
+
+import offerItem.OfferItem;
+
+public interface OffersInterface {
+	
+	HashMap<Integer, OfferItem> getAllOffers();
+	OfferItem getSingleOffer(int offerId);
+	OfferItem addOffer (int offerId, int lotId, double offerPrice);
+	void deleteOffer(int offerId);
+	void deleteOffersOfLot(int lotId);
+	void updateOfferPrice(int offerId, double offerPrice);
+	void updateSingleOfferStatus(int offerId, int offerStatus);
+	void updateOfferStatusOfLot(int lotId, int offerStatus);
+	 
+	
+}
